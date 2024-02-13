948838d056f46df94be089363843254acc215676
shalapai99 <159139237+shalapai99@users.noreply.github.com>
2024-02-13 17:49:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+
 let tg = window.Telegram.WebApp;
-tg.expand();
 
 $(document).on("click", "#buythis", function(){
         $(".listcontainer").css("display", "none");
+        $(".header").css("display", "none");
         $("#form").css("display", "block");
     });
 
     $(document).on("click", "#order", function(){
- name = $("#uname").val();
- mail = $("#mail").val();
- phone = $("#uphone").val();
+        let name = $("#uname").val();
+        let mail = $("#mail").val();
+        let phone = $("#uphone").val();
         
         if (name.length < 3) {
             alert("ошибка в имени");
@@ -21,17 +22,13 @@ $(document).on("click", "#buythis", function(){
             alert("ошибка в номере телефона");
         }
 
-        //$(".listcontainer").css("display", "grid");
-        //$("#form").css("display", "none");
-   
-    let data = {
-    name: name,
-    email: mail,
-    phone: phone
-};
-
-tg.sendData(JSON.stringify(data));
-tg.close;
+        let data = {
+            name: name,
+            email: mail,
+            phone: phone
+        };
+        
+        tg.sendData(JSON.stringify(data));
+        tg.close;
     });
-
 
