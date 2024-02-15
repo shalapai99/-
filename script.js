5dc001022a20ec86b0088ced0edcb9baa68edd2e
shalapai99 <159139237+shalapai99@users.noreply.github.com>
2024-02-15 21:54:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let tg = window.Telegram.WebApp;
+var tg = window.Telegram.WebApp;
 
 $(document).on("click", "#buythis", function(){
         $(".listcontainer").css("display", "none");
@@ -7,21 +7,24 @@ $(document).on("click", "#buythis", function(){
     });
 
     $(document).on("click", "#order", function(){
-        let name = $("#uname").val();
-        let mail = $("#mail").val();
-        let phone = $("#uphone").val();
+        var name = $("#uname").val();
+        var mail = $("#mail").val();
+        var phone = $("#uphone").val();
         
         if (name.length < 3) {
             alert("ошибка в имени");
+            return;
         }
         if (mail.length < 3) {
             alert("ошибка в Email");
+            return;
         }
         if (phone.length < 3) {
             alert("ошибка в номере телефона");
+            return;
         }
 
-        let data = {
+        var data = {
             name: name,
             email: mail,
             phone: phone
